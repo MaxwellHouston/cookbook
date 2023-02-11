764b753f7c35dfeb2a3ef4a0f265f9469623fe66
Max <maxhouston@gmail.com>
2023-02-11 13:10:20 -0500

    formatting

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -3,13 +3,7 @@ export interface INavBar {
 }
 
 const NavBar: React.FC<INavBar> = ({ sampleProp }) => {
-  return (
-    <nav
-      className="bg-black h-12 text-white"
-    >
-      {sampleProp}
-    </nav>
-  );
+  return <nav className="bg-black h-12 text-white">{sampleProp}</nav>;
 };
 
 export default NavBar;
