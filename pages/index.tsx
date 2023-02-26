44be734f4fce3521cfbbf4ed0b65fdc7f78abc6a
Max <maxhouston@gmail.com>
2023-02-26 11:25:08 -0500

    Set up landing page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,27 @@
+import { auth } from '@/lib/firebase/config';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div>Heres the home page!!!</div>;
+  const [user, loading] = useAuthState(auth);
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="">
+          <h1 className="text-5xl font-bold">Hillary&apos;s Cookbook</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          {!user && <button className="btn btn-primary">Log In</button>}
+          {!user && <div className="divider mx-auto w-1/2">or</div>}
+          <button className="btn btn-primary">View Recipes</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
